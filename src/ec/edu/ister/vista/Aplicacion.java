017408a4d9da75bbc165f7a9310df2020930b22d
Alfonso <Alfonso@SENSHISLARK>
2017-11-16 22:39:43 -0500

    DEBER 2 ESTRUCTURA DE DATOS

---
+++ src/ec/edu/ister/vista/Aplicacion.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.ister.vista;
+
+import ec.edu.ister.modelo.Ejercicios_Deber3;
+
+/**
+ *
+ * @author Alfonso
+ */
+public class Aplicacion {
+     public static void main(String[] args) {
+        Ejercicios_Deber3.menu();
+        Ejercicios_Deber3.ejercicio1();
+        Ejercicios_Deber3.ejercicio2();
+        Ejercicios_Deber3.ejercicio3();
+        Ejercicios_Deber3.ejercicio4();
+        Ejercicios_Deber3.ejercicio5();
+        Ejercicios_Deber3.ejercicio6();
+        
+    }
+    
+}
